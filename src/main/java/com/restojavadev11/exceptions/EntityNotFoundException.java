4f3e6ca1b8712cc1b32ca028956619c359a0dc72
Juan Alegre <jpalegree@gmail.com>
2023-05-08 21:53:45 -0300

    Refactor Exceptions to add loggers

--- src/main/java/com/restojavadev11/exceptions/EntityNotFoundException.java
+++ src/main/java/com/restojavadev11/exceptions/EntityNotFoundException.java
@@ -1,7 +1,12 @@
 package com.restojavadev11.exceptions;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EntityNotFoundException extends RuntimeException {
+    private static final Logger logger = Logger.getLogger(EntityNotFoundException.class.getName());
     public EntityNotFoundException(String message) {
         super(message);
+        logger.log(Level.SEVERE, message);
     }
 }
