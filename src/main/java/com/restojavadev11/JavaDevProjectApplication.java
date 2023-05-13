2a051405b199ade4cd5f94c4839f99080336abea
HazeelRamirez <hazeelhr11@gmail.com>
2023-05-12 18:57:00 -0600

    My first commit

--- src/main/java/com/restojavadev11/JavaDevProjectApplication.java
+++ src/main/java/com/restojavadev11/JavaDevProjectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaDevProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaDevProjectApplication.class, args);
+		SpringApplication.run(
+				JavaDevProjectApplication.class, args);
 
 	}
 
