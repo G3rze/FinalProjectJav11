90208e015d52d8048708972dc9e0ff173a39ab70
HazeelRamirez <hazeelhr11@gmail.com>
2023-05-15 15:17:22 -0600

    commit 1

--- src/main/java/com/restojavadev11/controller/TableController.java
+++ src/main/java/com/restojavadev11/controller/TableController.java
@@ -1,11 +1,13 @@
 package com.restojavadev11.controller;
 
+import com.restojavadev11.entity.PromotionEntity;
 import com.restojavadev11.entity.TableEntity;
 import com.restojavadev11.parameters.TableParameters;
 import com.restojavadev11.service.ITableService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,16 +24,19 @@ public class TableController {
     }
 
     @GetMapping("/getAll")
+    @PreAuthorize("hasAnyRole('WORKER', 'ADMIN', 'CUSTOMER') or hasAnyAuthority('SCOPE_ROLE_WORKER', 'SCOPE_ROLE_ADMIN', 'SCOPE_ROLE,CUSTOMER')")
     public List<TableEntity> listTables(){
         return tableService.allTables();
     }
 
     @GetMapping("/getById{id}")
+    @PreAuthorize("hasAnyRole('WORKER', 'ADMIN', 'CUSTOMER') or hasAnyAuthority('SCOPE_ROLE_WORKER', 'SCOPE_ROLE_ADMIN', 'SCOPE_ROLE,CUSTOMER')")
     public Optional<TableEntity> getTableById(@RequestParam("id") long id){
         return tableService.getTableById(id);
     }
 
     @PostMapping("/insertTables")
+    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyAuthority('SCOPE_ROLE_ADMIN')")
     public void insertTable(@RequestBody TableParameters tableParameters){
         System.out.println(tableParameters.getCapacity());
         try {
@@ -44,10 +49,5 @@ public class TableController {
         }catch (Exception e){
             new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    @DeleteMapping("/delete{id}")
-    public void deleteTableById(@RequestParam("id") long id){
-        tableService.deleteTable(id);
     }
 }
